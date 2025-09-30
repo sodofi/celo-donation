b4ebc9ecebdd5e13bd4814cde4ef1e1d3fb28be0
sodofi <sophiahdew@gmail.com>
2025-09-29 17:27:41 -0700

    fix build errors

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const PROJECT_TITLE = "Celo Donations";
 export const PROJECT_DESCRIPTION = "Send donations with USDC on Celo";
 export const PROJECT_CREATOR = "sophia";
-export const PROJECT_AVATAR_URL = "/hero.png";
+export const PROJECT_AVATAR_URL = `${process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://localhost:3000'}/hero.png`;
